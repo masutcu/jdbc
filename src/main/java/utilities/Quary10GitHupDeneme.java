a01525226979ac85a730c0529ab3736578c0e041
masutcu <masutcu@gmail.com>
2023-03-15 09:48:10 +0300

    v3

--- src/main/java/utilities/Quary10GitHupDeneme.java
+++ src/main/java/utilities/Quary10GitHupDeneme.java
@@ -3,5 +3,6 @@ package utilities;
 public class Quary10GitHupDeneme {
     public static void main(String[] args) {
         System.out.println("Deneme");
+        System.out.println("Deneme2");
     }
 }
